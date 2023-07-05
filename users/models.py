5bd2b654af58b13a7dab4e30d4d84617e19b9b0e
Miguel <miguelangeloliveira95@gmail.com>
2023-07-05 14:44:08 -0300

    feat: User: model, serializers, urls

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    full_name = models.CharField(max_length=128)
+    is_employee = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
