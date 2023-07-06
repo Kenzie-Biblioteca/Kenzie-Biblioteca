46186c0e8387305db825580ec73dc9d0b7c1c400
DMCFaria <danielmello0110@gmail.com>
2023-07-06 17:09:19 -0300

    fix:setting/User&&copys

--- copys/views.py
+++ copys/views.py
@@ -9,19 +9,13 @@ from books.models import Book
 from rest_framework import generics
 
 
-class ExtensionPagination(PageNumberPagination):
-    page_size = 1
-
-
 class CopyViews(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Copy.objects.all()
-    serializer_class = CopySerializer(queryset, many=True)
-
-    pagination_class = ExtensionPagination
+    serializer_class = CopySerializer
 
     def perform_create(self, serializer):
-        find_books = get_object_or_404(Books, id=self.kwargs.get("pk"))
+        find_books = get_object_or_404(Book, id=self.kwargs.get("pk"))
         serializer.save(Books=find_books)
